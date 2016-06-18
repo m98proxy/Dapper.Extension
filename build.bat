c78a43c265716301c4790eafd905a107fc3ce10f
m98proxy <m98proxy@gmail.com>
2016-06-18 03:13:22 -0300

    removed

--- build.bat
+++ build.bat
@@ -17,7 +17,9 @@ if "%nuget%" == "" (
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Dapper.Extensions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
+
 mkdir Build\lib
+
 mkdir Build\lib\net45
 
 %nuget% pack "Dapper.Extensions.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
